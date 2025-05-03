efff05367018f7bcda14027fd02f0d2932e79940
chrishmika <shehanchrishmika123@gmail.com>
2025-05-03 20:16:44 +0530

    Changes to be committed: 	modified:   backend/controllers/auth.controller.mjs 	modified:   backend/db/connectMongoDB.mjs 	new file:   backend/lib/utils/generateToken.mjs 	modified:   backend/server.mjs

--- backend/controllers/auth.controller.mjs
+++ backend/controllers/auth.controller.mjs
@@ -1,16 +1,103 @@
+import User from "../models/user.model.mjs";
+import { generateTokenAndSetCookie } from "../lib/utils/generateToken.mjs";
+import bcrypt from "bcryptjs";
+
 export const signup = async (req, res) => {
   try {
-  } catch (error) {}
+    const { fullname, username, email, password } = req.body;
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ error: "invalid email format need to be more than 6 chars" });
+    }
+
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ error: "user name already taken" });
+    }
+
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+      return res.status(400).json({ error: "user email already exists" });
+    }
+
+    if (password.length < 6) {
+      return res.status(400).json({ error: "invalid password format" });
+    }
+    //hashing password
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullname,
+      username,
+      email,
+      password: hashPassword,
+    });
+
+    if (newUser) {
+      generateTokenAndSetCookie(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullname: newUser.fullname,
+        username: newUser.username,
+        email: newUser.email,
+        followers: newUser.followers,
+        following: newUser.following,
+        profileImg: newUser.profileImg,
+        coverImg: newUser.coverImg,
+        bio: newUser.bio,
+        link: newUser.link,
+      });
+    } else {
+      return res.status(400).json({ error: "invalid user data" });
+    }
+  } catch (error) {
+    console.log(`error in signup controller ${error.message}`);
+
+    return res.status(500).json({ error: "internal server error on signup" });
+  }
 };
 
 export const login = async (req, res) => {
-  res.json({
-    data: "you are in login router",
-  });
+  try {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username });
+
+    const validation = await bcrypt.compare(password, user.password || "");
+
+    if (!validation || !user) {
+      return res.status(400).json({ error: "invalid credentials" });
+    }
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(201).json({
+      _id: user._id,
+      fullname: user.fullname,
+      username: user.username,
+      email: user.email,
+      followers: user.followers,
+      following: user.following,
+      profileImg: user.profileImg,
+      coverImg: user.coverImg,
+      bio: user.bio,
+      link: user.link,
+    });
+  } catch (error) {
+    console.log(`error in login controller ${error.message}`);
+    return res.status(500).json({ error: "internal server error on login" });
+  }
 };
 
 export const logout = async (req, res) => {
-  res.json({
-    data: "you are in login router",
-  });
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "user logout sucessfull" });
+  } catch (error) {
+    console.log(`error in login controller ${error.message}`);
+    return res.status(500).json({ error: "internal server error on logout" });
+  }
 };
--- backend/server.mjs
+++ backend/server.mjs
@@ -5,8 +5,12 @@ import router from "./routes/auth.routes.mjs";
 import connectMongoDB from "./db/connectMongoDB.mjs";
 
 dotenv.config();
+
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+app.use(express.json()); //to parse req.body
+app.use(express.urlencoded({ extended: true })); //to parse data from urlencoded
 
 app.use("/api/auth", router);
 
