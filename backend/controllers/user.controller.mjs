04fbd1fe6021f1ec7bdd1ac6c14c1d1f86e39352
chrishmika <shehanchrishmika123@gmail.com>
2025-05-06 01:35:47 +0530

    getSuggestedUsers

--- backend/controllers/user.controller.mjs
+++ backend/controllers/user.controller.mjs
@@ -56,9 +56,30 @@ export const followUnfollowUser = async (req, res) => {
 
 export const getSuggestedUsers = async (req, res) => {
   try {
+    const userId = req.user._id;
+
+    const usersFollowedByMe = await User.findById(userId).select("following");
+
+    const users = await User.aggregate([
+      {
+        $match: {
+          _id: { $ne: userId },
+        },
+      },
+      {
+        $sample: { size: 10 },
+      },
+    ]);
+
+    //from above it gives 10 users and then o filter existing followers and take first 4 of that filtered values
+    const filteredUsers = users.filter((user) => !usersFollowedByMe.following.includes(user._id));
+    const suggestedUsers = filteredUsers.slice(0, 4);
+
+    suggestedUsers.forEach((user) => (user.password = null));
+    res.status(200).json(suggestedUsers);
   } catch (error) {
     console.log(`error in userController ${error.message}`);
-    req.status(500).json({ error: "internal server error on getSuggestedUsers" });
+    res.status(500).json({ error: "internal server error on getSuggestedUsers" });
   }
 };
 
