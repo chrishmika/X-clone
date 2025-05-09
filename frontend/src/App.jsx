0db66d1ce3353a45c8c3b34495dd9a514bba8026
chrishmika <shehanchrishmika123@gmail.com>
2025-05-09 22:00:10 +0530

    UI design completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,9 @@ import HomePage from "./pages/home/HomePage";
 import LoginPage from "./pages/auth/login/LoginPage";
 import SignupPage from "./pages/auth/signup/SignUpPage";
 import Sidebar from "./components/common/Sidebar";
+import RightPannel from "./components/common/RightPannel";
+import NotificationPage from "./pages/notification/NotificationPage";
+import ProfilePage from "./pages/profile/ProfilePage";
 
 function App() {
   return (
@@ -15,7 +18,10 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/notifications" element={<NotificationPage />} />
+        <Route path="/profile/:id" element={<ProfilePage />} />
       </Routes>
+      <RightPannel />
     </div>
   );
 }
