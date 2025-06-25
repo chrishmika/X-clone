9d1f4242c9b62c68099c994106fd65f24493c6e0
chrishmika <shehanchrishmika123@gmail.com>
2025-06-26 01:56:35 +0530

    configerations set

--- backend/server.mjs
+++ backend/server.mjs
@@ -9,6 +9,7 @@ import userRouter from "./routes/user.route.mjs";
 import authRouter from "./routes/auth.route.mjs";
 import postRouter from "./routes/post.route.mjs";
 import notificationRoutes from "./routes/notification.route.mjs";
+import path from "path";
 
 dotenv.config();
 
@@ -18,8 +19,9 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const PORT = process.env.PORT || 5000;
 const app = express();
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "5mb" })); //to parse req.body
 app.use(express.urlencoded({ extended: true })); //to parse data from urlencoded
@@ -29,6 +31,15 @@ app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/post", postRouter);
 app.use("/api/notification", notificationRoutes);
+
+//this is used to send frontend endpoint request to frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  //this is used to send frontend endpoint request to frontend
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => console.log(`server is running on ${PORT}`));
 connectMongoDB();
